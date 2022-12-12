8bd23f0bcd4d946a235ba15503ecd8885874b1f3
Mihir3070 <93573109+Mihir3070@users.noreply.github.com>
2022-12-12 09:53:16 +0530

    First commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// JS Which needs to add
+document.write(	
+	'<script src="js/jquery.js"></script>' +
+	// '<script src="js/bootstrap.bundle.min.js"></script>' +
+	'<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>' +
+	'<script src="js/bootstrap-datepicker.min.js"></script>' +
+	'<script src="js/slick.min.js"></script>' +
+	'<script src="js/aos.js"></script>'+
+	'<script src="js/tilt.js"></script>'+
+	'<script src="js/custom.js"></script>'
+	);
